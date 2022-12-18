5cff9a5103569903feb87245fb988a3af94c3780
tintkoko <tintkoko.tko@gmail.com>
2022-12-18 12:55:20 +0700

    t7

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-const abc = 'asdf';
-
 app.get('/', (req, res) => {
-  res.send('Hello World!', abc);
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
