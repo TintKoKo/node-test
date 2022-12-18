c8ee5e97a2c2a38e87bf852521c15fd770fe7237
tintkoko <tintkoko.tko@gmail.com>
2022-12-18 12:49:19 +0700

    t5

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,6 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'import/prefer-default-export': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
 };
